abd988194e0450795612a0fac57fe6bd4b0fed60
robinmin <luonbin@hotmail.com>
2019-10-22 13:09:02 +0800

    Add filters on form fields

--- flashboard/forms.py
+++ flashboard/forms.py
@@ -2,8 +2,10 @@ from wtforms import StringField, BooleanField, PasswordField, SubmitField, Hidde
 from wtforms.validators import DataRequired, Length, Email
 from flask_wtf import FlaskForm
 
-
+from .utils import strip_space, strip_space_lower
 ###############################################################################
+
+
 class LoginForm(FlaskForm):
     # The first parameter is for form label
     # username = StringField('User Name', validators=[
@@ -15,11 +17,11 @@ class LoginForm(FlaskForm):
         DataRequired(message='Email is required'),
         Length(3, 64),
         Email(message='Please input valid email, e.g. : username@domain.com'),
-    ])
+    ], filters=[strip_space_lower])
     password = PasswordField('Password', validators=[
         DataRequired(message='Password is required'),
         Length(6, 12),
-    ])
+    ], filters=[strip_space])
     remember_me = BooleanField('Remember me', default=False)
     submit = SubmitField('Login')
 
@@ -29,18 +31,18 @@ class SignupForm(FlaskForm):
     name = StringField('User Name', validators=[
         DataRequired(message='User Name is required'),
         Length(3, 64),
-    ])
+    ], filters=[strip_space])
     email = StringField('Email', validators=[
         DataRequired(message='Email is required'),
         Length(3, 64),
         Email(message='Please input valid email, e.g. : username@domain.com'),
-    ])
+    ], filters=[strip_space_lower])
     password = PasswordField('Password', validators=[
         DataRequired(message='Password is required'),
         Length(6, 12),
-    ])
+    ], filters=[strip_space])
     password2 = PasswordField('Repeated Password', validators=[
         DataRequired(message='Repeated password is required too'),
         Length(6, 12),
-    ])
+    ], filters=[strip_space])
     submit = SubmitField('Sign-up')
