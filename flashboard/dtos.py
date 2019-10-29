63ca87866de8cc4d340209339032c3a178b6e56d
robinmin <luonbin@hotmail.com>
2019-10-29 23:58:53 +0800

    add i18n support

--- flashboard/dtos.py
+++ flashboard/dtos.py
@@ -1,29 +1,30 @@
 from flask_restplus import Namespace, fields
+from flask_babel import gettext as _
 
 
 class AppDTO:
-    api = Namespace('app', description='application related operations')
+    api = Namespace('app', description=_('application related operations'))
 
     return_message = api.model('return_message', {
-        'message': fields.String(description='message')
+        'message': fields.String(description=_('message'))
     })
 
     auth_details = api.model('auth_details', {
-        'email': fields.String(required=True, description='email address'),
-        'password': fields.String(required=True, description='user password '),
+        'email': fields.String(required=True, description=_('email address')),
+        'password': fields.String(required=True, description=_('user password ')),
     })
 
     return_token = api.model('return_token', {
-        'access_token': fields.String(required=True, description='API access token'),
-        'refresh_token': fields.String(required=True, description='API refresh token'),
+        'access_token': fields.String(required=True, description=_('API access token')),
+        'refresh_token': fields.String(required=True, description=_('API refresh token')),
     })
 
     refresh_details = api.model('refresh_details', {
-        'refresh_token': fields.String(required=True, description='API refresh token'),
+        'refresh_token': fields.String(required=True, description=_('API refresh token')),
     })
 
     register_details = api.model('register_details', {
-        'name': fields.String(required=True, description='user name'),
-        'email': fields.String(required=True, description='email address'),
-        'password': fields.String(required=True, description='password'),
+        'name': fields.String(required=True, description=_('user name')),
+        'email': fields.String(required=True, description=_('email address')),
+        'password': fields.String(required=True, description=_('password')),
     })
