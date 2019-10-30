c28bbd4042d1497dae095d0050725a0abbd56413
robinmin <luonbin@hotmail.com>
2019-10-30 16:03:23 +0800

    add default option for celery

--- config/config.py
+++ config/config.py
@@ -87,8 +87,11 @@ class Config(Settings):
     #  Enable features -- celery
     # --------------------------------------------------------------------------
     ENABLE_CELERY = False
-    CELERY_BROKER_URL = 'redis://localhost:6379',
-    CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+    CELERY_BROKER_URL = 'redis://localhost:6379/0',
+    CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+    CELERY_ACCEPT_CONTENT = ['json', 'msgpack', 'yaml']
+    CELERY_TASK_SERIALIZER = 'json'
+    CELERY_RESULT_SERIALIZER = 'json'
 
     # --------------------------------------------------------------------------
     #  Enable features -- sentry
