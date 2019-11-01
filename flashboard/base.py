475a165d272eb73fbebe59afaf5c8f4585ec4d84
robinmin <luonbin@hotmail.com>
2019-11-01 17:23:21 +0800

    Add ModelMixin

--- flashboard/base.py
+++ flashboard/base.py
@@ -2,6 +2,10 @@
     This file will contain all base classes here.
 """
 
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey  # noqa: F401
+from sqlalchemy.orm import relationship, backref  # noqa: F401
+from sqlalchemy.sql import func
+
 from sqlalchemy.ext.declarative import declarative_base
 
 from .utils import flatten
@@ -9,6 +13,15 @@ from .utils import flatten
 ###############################################################################
 # base class for all models
 BaseModel = declarative_base()
+
+
+class ModelMixin():
+    creator_id = Column('N_CREATOR_ID', Integer(), nullable=False)
+    owner_id = Column('N_OWNER_ID', Integer(), nullable=False)
+    in_use = Column('N_INUSE', Integer(), nullable=False, default=1)
+    create_on = Column('D_CREATE', DateTime(),
+                       nullable=False, default=func.now())
+    update_on = Column('D_UPDATE', DateTime(), nullable=False)
 
 
 class FormMixin(object):
