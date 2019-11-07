bdec20354dbb76b99ecd99c784563869d687a02e
robinmin <luonbin@hotmail.com>
2019-11-07 14:31:36 +0800

    specify column and table name of models

--- knowall/models.py
+++ knowall/models.py
@@ -2,41 +2,66 @@ from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey  #
 from sqlalchemy.orm import relationship, backref  # noqa: F401
 # from sqlalchemy.sql import func
 
-from .base import BaseModel, ModelMixin
+from flashboard.base import BaseModel, ModelMixin
+from flashboard.models import UserModel
 ###############################################################################
 
 
-class Project(BaseModel, ModelMixin):
-    __tablename__ = 'KA_PROJECTS'
+class ProjectModel(BaseModel, ModelMixin):
+    __tablename__ = 'ka_projects'
 
-    id = Column('N_PROJ_ID', Integer(), primary_key=True, autoincrement=True)
-    name = Column('C_NAME', String(80), unique=True, nullable=False)
-    description = Column('C_DESC', String(255))
+    id = Column('n_proj_id', Integer(), primary_key=True,
+                autoincrement=True, comment='ID')
+    name = Column('c_name', String(80), unique=True,
+                  nullable=False, comment='Name')
+    description = Column('c_desc', String(255), comment='Description')
 
-
-# class DBVarsModel(BaseModel):
-#     __tablename__ = 'DB_VARS'
-#     # - id
-#     # - table_id
-#     # - name
-#     # - display name
-#     # - external name
-#     # - source table
-#     # - source column
-#     # - biz_category
-#     # - data_type
-#     # - logic_type
-#     # - description
-#     # - url_detail
+    owner_id = Column('n_owner_id', Integer(), ForeignKey(
+        'sys_user.n_user_id'), comment='Owner ID')
 
 
-# class DBTableModel(BaseModel):
-#     __tablename__ = 'DB_TABLES'
-#     # - id
+class TableModel(BaseModel, ModelMixin):
+    __tablename__ = 'ka_tables'
+
+    id = Column('n_table_id', Integer(), primary_key=True,
+                autoincrement=True, comment='ID')
+    name = Column('c_name', String(80), unique=True,
+                  nullable=False, comment='Name')
+    description = Column('c_desc', String(
+        255), comment='Description')
+
+    proj_id = Column('n_proj_id', Integer(),
+                     ForeignKey('ka_projects.n_proj_id'), comment='Project ID')
+    owner_id = Column('n_owner_id', Integer(), ForeignKey(
+        UserModel.id), comment='Owner ID')
+    owner = relationship(UserModel, foreign_keys=owner_id)
+    project = relationship(ProjectModel, foreign_keys=proj_id)
 
 
-# class DBProjectModel(BaseModel):
-#     __tablename__ = 'DB_PROJECTS'
-#     # - id
+class ColumnModel(BaseModel, ModelMixin):
+    __tablename__ = 'ka_columns'
 
-###############################################################################
+    id = Column('n_col_id', Integer(), primary_key=True,
+                autoincrement=True, comment='ID')
+    name = Column('c_name', String(80), unique=True,
+                  nullable=False, comment='Name')
+    label = Column('c_label', String(80), nullable=False, comment='Label')
+    external_name = Column('c_extrn_name', String(80), comment='External name')
+    internal_name = Column('c_intrn_name', String(80), comment='Internal name')
+    src_table = Column('c_src_tab', String(
+        80), nullable=False, comment='Source table')
+    src_col = Column('c_src_col', String(
+        80), nullable=False, comment='Source column')
+    src_type = Column('c_src_type', String(
+        80), nullable=False, comment='Source type')
+    biz_category = Column('c_biz_category', String(80),
+                          comment='Business category')
+    logic_type = Column('c_logic_type', String(80),  comment='Logic type')
+    description = Column('c_desc', String(
+        255), comment='Description')
+    comments = Column('c_comments', String(
+        255), comment='Comments')
+
+    table_id = Column('n_table_id', Integer(),
+                      ForeignKey('ka_tables.n_table_id'), comment='Table ID')
+    table = relationship(TableModel, foreign_keys=table_id)
